30ad2b7e2849883b26c32c80ebb8bd031a0de480
Hamza Khalil <142446186+hamzathekhan@users.noreply.github.com>
2024-10-11 17:23:29 +0500

    Update Social Ad buyer with logistic regression serielization using joblib.py

--- joblib/Social Ad buyer with logistic regression serielization using joblib.py
+++ joblib/Social Ad buyer with logistic regression serielization using joblib.py
@@ -1,5 +1,32 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+df = pd.read_csv("/content/Social_Network_Ads.csv")
+df.info()
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1,test_size=0.2,random_state=42)
+for train_index , test_index in split.split(df,df["Purchased"]):
+  train_set = df.iloc[train_index]
+  test_set = df.iloc[test_index]
 
 
+from sklearn.linear_model import LogisticRegression
+
+model = LogisticRegression()
+
+model.fit(train_set[["Age","EstimatedSalary"]],train_set["Purchased"])
+
+
+model.predict([[40,105000]])
+
+
+# Serielization
 import joblib
 joblib.dump(model,"model_joblib")
 mj = joblib.load("/content/model_joblib")
